4dba8f724967a5e8c4eda759b26e7890474e9725
Ben Cochran <bcochran@gmail.com>
2010-01-05 01:05:20 -0600

    Add a `urlsafe` method

--- htmltools.py
+++ htmltools.py
@@ -1,6 +1,11 @@
+import re
+
 def htmlspecialchars(string):
     '''
     Simple copy of PHP's htmlspecialchars
     '''
     return string.replace('&', '&amp;').replace('"', '&quot;')\
         .replace("'", '&#039;').replace('<', '&lt;').replace('>', '&gt;')
+
+def urlsafe(string):
+    return re.sub('\W', '_', string)
