70ae69a36d9662b7ac83d327cba1a80798ace212
Ben Cochran <bcochran@gmail.com>
2010-01-05 01:10:18 -0600

    Filter out many more unsupported tags

--- templater.py
+++ templater.py
@@ -1,6 +1,7 @@
 '''
 The Tumblr templater. Handles rendering a template for a Tumblelog object.
 '''
+import re
 
 from blocks import match_block, filter_block
 from tumblelog import post_is_type
@@ -39,20 +40,40 @@ def render(blog, template):
             content = content + render_single_post(post, template)
         return content
 
+    ignored_blocks = ['SearchPage', 'PostSummary', 'NextPage', \
+        'PermalinkPage', 'PostTitle', 'PostSummary', 'PermalinkPagination', \
+        'PreviousPost', 'NextPost', 'GroupMembers', 'GroupMember', 'DayPage', \
+        'DayPagination', 'PreviousDayPage', 'NextDayPage', 'TagPage', \
+        'SearchPage', 'NoSearchResults', 'Following', 'Followed', 'Likes', \
+        'Twitter']
+    
+    ignored_tags = ['NextPage', 'PostTitle', 'PostSummary', 'PreviousPost', \
+        'NextPage', 'Favicon', 'CustomCSS', 'GroupMemberName', \
+        'GroupMemberTitle', 'GroupMemberURL', 'GroupMemberPortraitURL-16', \
+        'GroupMemberPortraitURL-24', 'GroupMemberPortraitURL-30', \
+        'GroupMemberPortraitURL-40', 'GroupMemberPortraitURL-48', \
+        'GroupMemberPortraitURL-64', 'GroupMemberPortraitURL-96', \
+        'GroupMemberPortraitURL-128', 'PreviousDayPage', 'NextDayPage', 'Tag',\
+        'URLSafeTag', 'TagURL', 'TagURLChrono', 'SearchQuery', \
+        'URLSaveSearchQuery', 'SearchResultCount', 'FollowedName', \
+        'FollowedTitle', 'FollowedURL', 'FollowedPortraitURL-16', \
+        'FollowedPortraitURL-20', 'FollowedPortraitURL-30', \
+        'FollowedPortraitURL-40', 'FollowedPortraitURL-48', \
+        'FollowedPortraitURL-64', 'FollowedPortraitURL-96', \
+        'FollowedPortraitURL-128', 'Likes', 'TwitterUserName']
+
+    for block in ignored_blocks:
+        template = filter_block(block, False, template)
+    
+    for tag in ignored_tags:
+        template = template.replace('{%s}' % tag, '')
+
+
     template = render_posts(template)
-    template = filter_block('SearchPage', False, template)
-    template = filter_block('PostSummary', False, template)
     template = filter_block('Description', blog.description, template)
-    template = filter_block('NextPage', False, template)
     template = filter_block('PreviousPage', True, template)
-    template = filter_block('PermalinkPage', False, template)
     template = filter_block('IndexPage', True, template)
-    template = filter_block('PostTitle', False, template)
-    template = filter_block('PostSummary', False, template)
     template = filter_block('Pagination', True, template)
-    template = filter_block('PermalinkPagination', False, template)
-    template = filter_block('PreviousPost', False, template)
-    template = filter_block('NextPost', False, template)
     
     template = template.replace('{Title}', blog.title)
     template = template.replace('{Description}', blog.description)
@@ -60,14 +81,11 @@ def render(blog, template):
         htmlspecialchars(blog.description))
     template = template.replace('{RSS}', '%s/rss' % blog.url)
     template = template.replace('{PreviousPage}', '%s/page/2' % blog.url)
-    template = template.replace('{NextPage}', '')
     template = template.replace('{CurrentPage}', '1')
     template = template.replace('{TotalPages}', '15')
-    template = template.replace('{PostTitle}', '')
-    template = template.replace('{PostSummary}', '')
-    template = template.replace('{PreviousPost}', '')
-    template = template.replace('{NextPost}', '')
-    template = template.replace('{Favicon}', '')
-    template = template.replace('{CustomCSS}', '')
+
+    template = re.sub('{Likes limit="\d+"}', '', template)
+    template = re.sub('{Likes width="\d+"}', '', template)
+    template = re.sub('{Likes summarize="\d+"}', '', template)
 
     return template
